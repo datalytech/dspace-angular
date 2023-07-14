ee9443913937d3020abd0b4994baf9aaf2144362
Enea Jahollari <enea.jahollari@4science.com>
2023-07-14 16:12:45 +0200

    added shared animations

---
+++ libs/shared/animations/src/lib/animations/animations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-animations',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './animations.component.html',
+  styleUrls: ['./animations.component.scss'],
+})
+export class AnimationsComponent {}
