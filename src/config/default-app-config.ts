b4cd4d9c62d5c5a9dcc566e771a0ed04fe29016b
George Veranis <gveranis@dataly.gr>
2024-10-21 14:12:09 +0200

    change session limits

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -124,15 +124,15 @@ export class DefaultAppConfig implements AppConfig {
     // Authentication UI settings
     ui: {
       // the amount of time before the idle warning is shown
-      timeUntilIdle: 15 * 60 * 1000, // 15 minutes
+      timeUntilIdle: 60 * 60 * 1000, // 60 minutes
       // the amount of time the user has to react after the idle warning is shown before they are logged out.
-      idleGracePeriod: 5 * 60 * 1000 // 5 minutes
+      idleGracePeriod: 15 * 60 * 1000 // 15 minutes
     },
     // Authentication REST settings
     rest: {
       // If the rest token expires in less than this amount of time, it will be refreshed automatically.
       // This is independent from the idle warning.
-      timeLeftBeforeTokenRefresh: 2 * 60 * 1000 // 2 minutes
+      timeLeftBeforeTokenRefresh: 5 * 60 * 1000 // 5 minutes
     }
   };
 
