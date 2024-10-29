d50821479ecdfe9e8b35c969ecc87409acc6b900
Kostis Alexandris <kostisalex@gmail.com>
2024-10-29 10:43:29 +0200

    Fixes in UU

--- src/app/shared/truncatable/truncatable-part/truncatable-part.component.ts
+++ src/app/shared/truncatable/truncatable-part/truncatable-part.component.ts
@@ -1,147 +1,159 @@
-import { AfterViewChecked, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { TruncatableService } from '../truncatable.service';
-import { hasValue } from '../../empty.util';
+  import { AfterViewChecked, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+  import { TruncatableService } from '../truncatable.service';
+  import { hasValue } from '../../empty.util';
 
-@Component({
-  selector: 'ds-truncatable-part',
-  templateUrl: './truncatable-part.component.html',
-  styleUrls: ['./truncatable-part.component.scss']
-})
-
-/**
- * Component that truncates/clamps a piece of text
- * It needs a TruncatableComponent parent to identify it's current state
- */
-export class TruncatablePartComponent implements AfterViewChecked, OnInit, OnDestroy {
-  /**
-   * Number of lines shown when the part is collapsed
-   */
-  @Input() minLines: number;
+  @Component({
+    selector: 'ds-truncatable-part',
+    templateUrl: './truncatable-part.component.html',
+    styleUrls: ['./truncatable-part.component.scss']
+  })
 
   /**
-   * Number of lines shown when the part is expanded. -1 indicates no limit
+   * Component that truncates/clamps a piece of text
+   * It needs a TruncatableComponent parent to identify it's current state
    */
-  @Input() maxLines = -1;
+  export class TruncatablePartComponent implements AfterViewChecked, OnInit, OnDestroy {
+    /**
+     * Number of lines shown when the part is collapsed
+     */
+    @Input() minLines: number;
 
-  /**
-   * The identifier of the parent TruncatableComponent
-   */
-  @Input() id: string;
+    /**
+     * Number of lines shown when the part is expanded. -1 indicates no limit
+     */
+    @Input() maxLines = -1;
 
-  /**
-   * Type of text, can be a h4 for headers or any other class you want to add
-   */
-  @Input() type: string;
+    /**
+     * The identifier of the parent TruncatableComponent
+     */
+    @Input() id: string;
 
-  /**
-   * True if the minimal height of the part should at least be as high as it's minimum amount of lines
-   */
-  @Input() fixedHeight = false;
+    /**
+     * Type of text, can be a h4 for headers or any other class you want to add
+     */
+    @Input() type: string;
 
-  @Input() background = 'default';
+    /**
+     * True if the minimal height of the part should at least be as high as it's minimum amount of lines
+     */
+    @Input() fixedHeight = false;
 
-  /**
-   * A boolean representing if to show or not the show/collapse toggle.
-   * This value must have the same value as the parent TruncatableComponent
-   */
-  @Input() showToggle = true;
+    @Input() background = 'default';
 
-  /**
-   * The view on the truncatable part
-   */
-  @ViewChild('content', {static: true}) content: ElementRef;
+    /**
+     * A boolean representing if to show or not the show/collapse toggle.
+     * This value must have the same value as the parent TruncatableComponent
+     */
+    @Input() showToggle = true;
 
-  /**
-   * Current amount of lines shown of this part
-   */
-  lines: string;
+    /**
+     * The view on the truncatable part
+     */
+    @ViewChild('content', {static: true}) content: ElementRef;
 
-  /**
-   * Subscription to unsubscribe from
-   */
-  private sub;
-  /**
-   * store variable used for local to expand collapse
-   */
-  expand = false;
-  /**
-   * variable to check if expandable
-   */
-  expandable = false;
+    /**
+     * Current amount of lines shown of this part
+     */
+    lines: string;
 
-  public constructor(private service: TruncatableService) {}
+    /**
+     * Subscription to unsubscribe from
+     */
+    private sub;
+    /**
+     * store variable used for local to expand collapse
+     */
+    expand = false;
+    /**
+     * variable to check if expandable
+     */
+    expandable = false;
 
-  /**
-   * Initialize lines variable
-   */
-  ngOnInit() {
-    this.setLines();
-  }
+    public constructor(private service: TruncatableService) {}
 
-  /**
-   * Subscribe to the current state to determine how much lines should be shown of this part
-   */
-  private setLines() {
-    this.sub = this.service.isCollapsed(this.id).subscribe((collapsed: boolean) => {
-      if (collapsed) {
-        this.lines = this.minLines.toString();
-        this.expand = false;
-      } else {
-        this.lines = this.maxLines < 0 ? 'none' : this.maxLines.toString();
-        this.expand = true;
-      }
-    });
-  }
+    /**
+     * Initialize lines variable
+     */
+    ngOnInit() {
+      this.setLines();
+    }
 
-  ngAfterViewChecked() {
-    this.truncateElement();
-  }
+    /**
+     * After the view is ready, search for strings that match an anchor tag pattern and replace with actual links
+     */
+    ngAfterViewInit() {
+      const anchorTagPatternToMatch = new RegExp(/&lt;a href="(.*?)"&gt;(.*?)&lt;\/a&gt;/g);
+      const contentElement = this.content.nativeElement;
+      contentElement.innerHTML = contentElement.innerHTML.replace(
+        anchorTagPatternToMatch,
+        '<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>'
+      );
+    }
 
-  /**
-   * Expands the truncatable when it's collapsed, collapses it when it's expanded
-   */
-  public toggle() {
-    this.service.toggle(this.id);
-    this.expandable = !this.expandable;
-  }
+    /**
+     * Subscribe to the current state to determine how much lines should be shown of this part
+     */
+    private setLines() {
+      this.sub = this.service.isCollapsed(this.id).subscribe((collapsed: boolean) => {
+        if (collapsed) {
+          this.lines = this.minLines.toString();
+          this.expand = false;
+        } else {
+          this.lines = this.maxLines < 0 ? 'none' : this.maxLines.toString();
+          this.expand = true;
+        }
+      });
+    }
 
-  /**
-   * check for the truncate element
-   */
-  public truncateElement() {
-    if (this.showToggle) {
-      const entry = this.content.nativeElement;
-      if (entry.scrollHeight > entry.offsetHeight) {
-        if (entry.children.length > 0) {
-          if (entry.children[entry.children.length - 1].offsetHeight > entry.offsetHeight) {
-            entry.classList.add('truncated');
-            entry.classList.remove('removeFaded');
+    ngAfterViewChecked() {
+      this.truncateElement();
+    }
+
+    /**
+     * Expands the truncatable when it's collapsed, collapses it when it's expanded
+     */
+    public toggle() {
+      this.service.toggle(this.id);
+      this.expandable = !this.expandable;
+    }
+
+    /**
+     * check for the truncate element
+     */
+    public truncateElement() {
+      if (this.showToggle) {
+        const entry = this.content.nativeElement;
+        if (entry.scrollHeight > entry.offsetHeight) {
+          if (entry.children.length > 0) {
+            if (entry.children[entry.children.length - 1].offsetHeight > entry.offsetHeight) {
+              entry.classList.add('truncated');
+              entry.classList.remove('removeFaded');
+            } else {
+              entry.classList.remove('truncated');
+              entry.classList.add('removeFaded');
+            }
           } else {
-            entry.classList.remove('truncated');
-            entry.classList.add('removeFaded');
+            if (entry.innerText.length > 0) {
+              entry.classList.add('truncated');
+              entry.classList.remove('removeFaded');
+            } else {
+              entry.classList.remove('truncated');
+              entry.classList.add('removeFaded');
+            }
           }
         } else {
-          if (entry.innerText.length > 0) {
-            entry.classList.add('truncated');
-            entry.classList.remove('removeFaded');
-          } else {
-            entry.classList.remove('truncated');
-            entry.classList.add('removeFaded');
-          }
+          entry.classList.remove('truncated');
+          entry.classList.add('removeFaded');
         }
-      } else {
-        entry.classList.remove('truncated');
-        entry.classList.add('removeFaded');
+      }
+    }
+
+    /**
+     * Unsubscribe from the subscription
+     */
+    ngOnDestroy(): void {
+      if (hasValue(this.sub)) {
+        this.sub.unsubscribe();
       }
     }
   }
-
-  /**
-   * Unsubscribe from the subscription
-   */
-  ngOnDestroy(): void {
-    if (hasValue(this.sub)) {
-      this.sub.unsubscribe();
-    }
-  }
-}
