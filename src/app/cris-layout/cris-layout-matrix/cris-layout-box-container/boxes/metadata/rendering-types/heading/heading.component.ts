e24672db30ddf48fcc283ca2565ebd0ce91851fb
George Veranis <gveranis@dataly.gr>
2024-11-09 01:46:46 +0100

    various changes for UI and translations

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/heading/heading.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/heading/heading.component.ts
@@ -13,11 +13,13 @@ import { RenderingTypeValueModelComponent } from '../rendering-type-value.model'
 @MetadataBoxFieldRendering(FieldRenderingType.HEADING)
 export class HeadingComponent extends RenderingTypeValueModelComponent {
   isOpenAccess = false;
+  isClosedAccess = false;
   uhType = undefined;
 
   ngOnInit() {
     if (this.item) {
       this.isOpenAccess = this.item.metadata['dc.rights']?.some(right => right.value === 'Open Access');
+      this.isClosedAccess = this.item.metadata['dc.rights']?.some(right => right.value === 'Closed Access');     
       const uhtype = this.item.metadata['dc.type.uhtype'];
       if (uhtype && uhtype[0] && uhtype[0].value) {
         this.uhType = uhtype[0].value;
