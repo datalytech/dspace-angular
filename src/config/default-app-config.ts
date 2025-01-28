26ea29a317a0c88cc0fe5ddcbe58d4442e61e357
George Veranis <gveranis@dataly.gr>
2025-01-28 08:54:00 +0100

    change translation, backgroung, justify text, lang

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -299,9 +299,8 @@ export class DefaultAppConfig implements AppConfig {
   // When set to active, users will be able to switch to the use of this language in the user interface.
   languages: LangConfig[] = [
     { code: 'en', label: 'English', active: true },
-    { code: 'el', label: 'Ελληνικά', active: true },
-    { code: 'fr', label: 'Français', active: true }
-      ];
+    { code: 'el', label: 'Ελληνικά', active: true }
+  ];
 
   // Browse-By Pages
   browseBy: BrowseByConfig = {
